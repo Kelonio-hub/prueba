dfb0af3e7617fb0538652f32c5ae7e39c98a0a92
Kelonio 3DS <67707384+Kelonio-hub@users.noreply.github.com>
2025-10-11 22:38:36 +0200

    Delete splash.js

--- splash.js
+++
@@ -1,6 +0,0 @@
-// Lógica para splash (timeout en initApp, pero separada para "splash")
-function hideSplash() {
-    const splash = document.getElementById('splash');
-    splash.classList.add('hidden');
-    setTimeout(function() { splash.remove(); }, 500);
-}
